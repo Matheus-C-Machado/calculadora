d4710632d111f5997eedf7d934740513735589c8
Matheus-C-Machado <158229786+Matheus-C-Machado@users.noreply.github.com>
2024-10-01 19:36:23 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+  <script>
+  import Calculadora from './components/Calculadora.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      Calculadora,
+    },
+  };
+</script>
+
+<template>
+  <div id="app">
+    <Calculadora />
+  </div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+  <footer> Matheus Costa</footer>
+</template>
+
+<style>
+</style>
